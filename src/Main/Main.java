aef64d93d09c0f9ef81fc47d68bdcd808653a4a9
Le Duong Hoang Dung <anniethehedgehog123@gmail.com>
2025-12-17 16:41:24 +0700

    Fix SOLID and bug

---
+++ src/Main/Main.java
@@ -0,0 +1,12 @@
+package Main;
+
+import UI.Menu;
+import javax.swing.SwingUtilities;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new Menu().setVisible(true);
+        });
+    }
+}
